d367d9f829872e2f2375ae2824216b50d9f91c6a
Yuxin Ye <yuxin_ye@protonmail.com>
2022-09-28 16:34:12 +1000

    add styling for SongCard

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,5 +1,24 @@
-const SongCard = () => (
-  <div>SongCard</div>
-);
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+import PlayPause from "./PlayPause";
+import { playPause, setActiveSong } from "../redux/features/playerSlice";
+
+const SongCard = ({ song, i }) => {
+  const activeSong = "Test";
+  return (
+    <div className="flex flex-col w-[250px] p-4 bg-white/10 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+      <div className="relative w-full h-56 group">
+        <div
+          className={`absolute rounded inset-0 justify-center items-center bg-white bg-opacity-40 group-hover:flex ${
+            activeSong?.title === song.title
+              ? "flex bg-white bg-opacity-70"
+              : "hidden"
+          }`}
+        ></div>
+      </div>
+    </div>
+  );
+};
 
 export default SongCard;
