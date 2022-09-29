112e2f59a54bd4b423f535487c51d21e98998017
Yuxin Ye <yuxin_ye@protonmail.com>
2022-09-29 13:35:49 +1000

    add sidebar styleing

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,8 @@
-import { loader } from "../assets";
+import {} from "../assets";
 
 const Loader = ({ title }) => (
   <div className="w-full flex justify-center items-center flex-col">
-    <img src={loader} alt="loader" className="w-32 h-32 object-contain" />
-    <h1 className="font-bold text-2xl mt-2 text-white">
+    <h1 className="font-bold text-2xl mt-44 text-white">
       {title || "Loading..."}
     </h1>
   </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,31 @@
-const Sidebar = () => (
-  <div>Sidebar</div>
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { RiCloseLine, RiMusic2Fill } from "react-icons/ri";
+
+import { logo } from "../assets";
+import { links } from "../assets/constants";
+
+const NavLinks = () => (
+  <div className="mt-10">
+    {links.map((item) => (
+      <NavLink className="ml-5 flex flex-row justify-start items-center my-8 text-white font-bold hover:text-black">
+        {item.name}
+      </NavLink>
+    ))}
+  </div>
 );
+
+const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <>
+      <div className="md:flex hidden flex-col w-[240PX] py-10 px-4 bg-[#eeaeca]">
+        <RiMusic2Fill className="text-white h-14 w-full" />
+        <NavLinks />
+      </div>
+    </>
+  );
+};
 
 export default Sidebar;
