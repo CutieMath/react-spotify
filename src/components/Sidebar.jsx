636ac098ee4996ce74ad70375cad3e9c4dee3252
Yuxin Ye <yuxin_ye@protonmail.com>
2022-09-30 16:51:09 +1000

    add mobile view for sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { HiOutlineMenu } from "react-icons/hi";
 import { RiCloseLine, RiMusic2Fill } from "react-icons/ri";
 
 import { logo } from "../assets";
 import { links } from "../assets/constants";
 
-const NavLinks = () => (
+const NavLinks = ({ handleClick }) => (
   <div className="mt-10">
     {links.map((item) => (
-      <NavLink className="ml-5 flex flex-row justify-start items-center my-8 text-white font-bold hover:text-black">
+      <NavLink
+        key={item.name}
+        to={item.to}
+        className="duration-500 ml-5 flex flex-row justify-start items-center my-8 text-white font-bold hover:text-gray-500"
+        onClick={() => handleClick && handleClick()}
+      >
+        <item.icon className="w-6 h-6 mr-2" />
         {item.name}
       </NavLink>
     ))}
@@ -23,6 +30,28 @@ const Sidebar = () => {
       <div className="md:flex hidden flex-col w-[240PX] py-10 px-4 bg-[#eeaeca]">
         <RiMusic2Fill className="text-white h-14 w-full" />
         <NavLinks />
+      </div>
+      {/* dedicated mobile menu */}
+      <div className="absolute md:hidden block top-6 right-3">
+        {mobileMenuOpen ? (
+          <RiCloseLine
+            className="w-10 h-10 text-white font-bold mr-2"
+            onClick={() => setMobileMenuOpen(false)}
+          />
+        ) : (
+          <HiOutlineMenu
+            className="w-10 h-10 text-white font-bold mr-2"
+            onClick={() => setMobileMenuOpen(true)}
+          />
+        )}
+      </div>
+
+      <div
+        className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#eeaeca] backdrop-blur-lg z-10 p-6 md:hidden 
+        ${mobileMenuOpen ? "left-0" : "-left-full"}`}
+      >
+        <RiMusic2Fill className="text-white h-14 w-full" />
+        <NavLinks handleClick={() => setMobileMenuOpen(false)} />
       </div>
     </>
   );
