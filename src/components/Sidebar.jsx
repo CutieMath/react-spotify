82cdfaf03746ebe4b892df1801fc75aee68ffafd
Yuxin Ye <yuxin_ye@protonmail.com>
2022-10-03 17:07:07 +1100

    change some styling for side bar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { HiOutlineMenu } from "react-icons/hi";
 import { RiCloseLine, RiMusic2Fill } from "react-icons/ri";
-
-import { logo } from "../assets";
 import { links } from "../assets/constants";
 
 const NavLinks = ({ handleClick }) => (
@@ -12,7 +10,7 @@ const NavLinks = ({ handleClick }) => (
       <NavLink
         key={item.name}
         to={item.to}
-        className="duration-500 ml-5 flex flex-row justify-start items-center my-8 text-white font-bold hover:text-gray-500"
+        className="duration-500 ml-5 flex flex-row justify-start items-center my-10 font-bold hover:text-gray-500"
         onClick={() => handleClick && handleClick()}
       >
         <item.icon className="w-6 h-6 mr-2" />
@@ -24,23 +22,22 @@ const NavLinks = ({ handleClick }) => (
 
 const Sidebar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
   return (
     <>
-      <div className="md:flex hidden flex-col w-[240PX] py-10 px-4 bg-[#eeaeca]">
-        <RiMusic2Fill className="text-white h-14 w-full" />
+      <div className="md:flex hidden flex-col w-[240PX] py-10 px-4 ">
+        <RiMusic2Fill className="text-black h-14 w-full" />
         <NavLinks />
       </div>
       {/* dedicated mobile menu */}
-      <div className="absolute md:hidden block top-6 right-3">
+      <div className="absolute md:hidden block top-6 right-3 ">
         {mobileMenuOpen ? (
           <RiCloseLine
-            className="w-10 h-10 text-white font-bold mr-2"
+            className="text-black w-10 h-10 font-bold mr-2"
             onClick={() => setMobileMenuOpen(false)}
           />
         ) : (
           <HiOutlineMenu
-            className="w-10 h-10 text-white font-bold mr-2"
+            className="text-black w-10 h-10 font-bold mr-2"
             onClick={() => setMobileMenuOpen(true)}
           />
         )}
@@ -50,7 +47,7 @@ const Sidebar = () => {
         className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#eeaeca] backdrop-blur-lg z-10 p-6 md:hidden 
         ${mobileMenuOpen ? "left-0" : "-left-full"}`}
       >
-        <RiMusic2Fill className="text-white h-14 w-full" />
+        <RiMusic2Fill className="text-black h-14 w-full" />
         <NavLinks handleClick={() => setMobileMenuOpen(false)} />
       </div>
     </>
